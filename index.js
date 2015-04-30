321a4e8c52597c162ba47d4d266ad49437b5173d
charly22 <carlos@olapic.com>
2015-04-30 14:22:19 -0300

    initial

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var api = require('./lib/api');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    next();
+});
+
+router.route('/')
+.get(function(req, res) {
+    api({})
+    .then(function(data){
+        res.json(data);
+    }).catch(function(error){
+        res.status(500).json(error);
+    });
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
+app.use(function(req, res){
+    res.status(400).json({code: 400, error: 'Bad request'});
+});
+app.listen(process.env.PORT || 3000);
